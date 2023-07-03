fac0f8e5cf2ae4ffe10260c3adf4b8290ece0c36
ericzakariasson <eric.zakariasson@gmail.com>
2023-07-03 21:13:43 +0200

    fix: initial page redirect

--- apps/web/src/components/InitialPageRedirect.tsx
+++ apps/web/src/components/InitialPageRedirect.tsx
@@ -2,52 +2,50 @@
 
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { pageService } from "../core/page/service";
-import { useAccount } from "wagmi";
-import { useEffect } from "react";
+
+import { useEffect, useRef } from "react";
+import { usePageService } from "../core/hooks/use-page-service";
 
 export function InitialPageRedirect() {
   const router = useRouter();
-  const account = useAccount();
+  const isRedirectingRef = useRef(false);
+
   const queryClient = useQueryClient();
 
-  const createPageMutation = useMutation(
-    async () => {
-      return await pageService(account.address!).create();
+  const pageService = usePageService();
+
+  const pagesQuery = useQuery({
+    queryKey: ["pages"],
+    queryFn: async () => pageService.getAll(),
+  });
+
+  const createPageMutation = useMutation({
+    mutationFn: async () => await pageService.create(),
+    onSuccess: (page) => {
+      queryClient.refetchQueries(["pages"]);
+      router.push(`/${page.localId}`);
     },
-    {
-      onSuccess: (page) => {
-        queryClient.refetchQueries(["pages"]);
-        router.push(`/${page.localId}`);
-      },
-    }
-  );
+  });
+
+  const pages = pagesQuery.data ?? [];
 
   useEffect(() => {
-    if (account.address) {
-      createPageMutation.mutate();
+    if (isRedirectingRef.current) {
+      return;
     }
-  }, [account.address]);
 
-  // const pagesQuery = useQuery(["pages"], async () => {
-  //   return await pageService.getAll();
-  // });
+    if (pages.length === 0) {
+      isRedirectingRef.current = true;
+      return createPageMutation.mutate();
+    }
 
-  // if (pagesQuery.data) {
-  //   const pages = pagesQuery.data ?? [];
+    if (pages.length > 0) {
+      const page = pages.at(0);
+      if (page) {
+        router.push(`/${page.localId}`);
+      }
+    }
+  }, [pages]);
 
-  //   if (pages.length === 0) {
-  //     createPageMutation.mutate();
-  //     return "loading...";
-  //   }
-
-  //   const page = pages.at(0);
-
-  //   if (page) {
-  //     router.push(`/${page.localId}`);
-  //     return "loading...";
-  //   }
-  // }
-
-  return "loading...";
+  return null;
 }
