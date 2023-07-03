0a7d3db231e2d44e8a9afdc2642ef2b78a71048d
ericzakariasson <eric.zakariasson@gmail.com>
2023-07-03 21:45:17 +0200

    chore: remove auth guard

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,19 +1,18 @@
 import { cn } from "@denoted/ui/src/utils";
 import { PropsWithChildren } from "react";
-import { Sidebar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
-import { AuthGuard } from "../../components/AuthGuard";
+import { Sidebar } from "../../components/Sidebar";
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
     <div className={cn("flex min-h-screen")}>
       <Sidebar className="fixed w-64" />
       <div className="grow pt-4 pl-64">
-        <Header className="absolute right-0 top-0 p-4" />
+        <Header className="absolute right-0 top-0 p-4 z-10" />
         <main
           className={cn("relative m-auto h-full max-w-3xl px-4 flex flex-col")}
         >
-          <AuthGuard>{children}</AuthGuard>
+          {children}
         </main>
       </div>
     </div>
